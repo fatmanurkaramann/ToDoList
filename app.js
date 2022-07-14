df3de7c41c0474e87fbed5a3fc223de479a01599
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2022-07-14 14:15:39 +0300

    todo app with local storage

---
+++ app.js
@@ -0,0 +1,103 @@
+const addToList = document.querySelector(".button")
+const toDoList = document.querySelector("#list")
+const text = document.querySelector("#task") //input
+
+const deleteToDo=(e)=>{
+    const todo=e.target.parentElement;
+    const text=todo.firstChild.textContent;
+    console.log(text)
+    let todos =JSON.parse(localStorage.getItem("todos")) 
+    todos=todos.filter(td=>td.text!=text)
+    todo.remove()
+
+    localStorage.setItem("todos",JSON.stringify(todos))
+
+   }
+   const doneToDo=(e)=>{
+  
+    const todo = e.target.parentElement
+    const text = todo.firstChild.textContent;
+ 
+    let todos = JSON.parse(localStorage.getItem("todos"));
+  
+    todos.forEach(todo => {
+       if (e.target.tagName=text) 
+       {todo.isCompleted = !todo.isCompleted 
+        
+   }
+   if(todo.isCompleted==true){
+    e.target.classList.toggle('checked')
+}
+console.log(todos)});
+    localStorage.setItem("todos", JSON.stringify(todos));
+    
+   
+
+}
+  
+const addItem = (todo) => { //todo
+let liDom = document.createElement("li")
+
+    liDom.innerHTML = `${todo.text}`
+    liDom.classList.add("list-group-item", "success");
+    toDoList.append(liDom)
+
+    let span=document.createElement("span")
+    let text=document.createTextNode("\u00D7")
+    span.className="close"
+    span.appendChild(text)
+    liDom.append(span)
+    toDoList.className = "";
+    span.addEventListener("click",deleteToDo)
+    liDom.addEventListener("click",doneToDo)
+
+
+  
+}
+
+const startConf = () => {
+    // baslangic ayarlari
+
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (!todos) {
+       localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+       todos.forEach(todo => {
+          addItem(todo);
+       });
+    } 
+ }
+
+function added(event) {
+    event.preventDefault()
+   
+    if (text.value == '')  { // boş değer girilmeye çalışıyor ise hata veriyoruz
+        text.style.border = '1px solid tomato';
+        setTimeout(() => {
+           text.style.borderColor = 'transparent';
+        }, 2500);
+        return false;
+     }
+   const todo={
+    text:text.value,
+    isCompleted:false
+   }
+    
+    const todos =JSON.parse(localStorage.getItem("todos")) 
+    todos.push(todo)
+    localStorage.setItem("todos",JSON.stringify(todos))
+    console.log(todos)
+
+    addItem(todo)
+    text.value=""
+ 
+
+}
+
+
+startConf();
+
+addToList.addEventListener("click", added)
+
+
+
